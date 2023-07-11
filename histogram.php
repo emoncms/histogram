7e1ee8c0680fa08ea395444f4d302e6f3bc3e5d9
TrystanLea <trystan.lea@gmail.com>
2023-07-11 12:31:42 +0100

    first commit

---
+++ histogram.php
@@ -0,0 +1,105 @@
+<?php
+
+class Histogram {
+    
+    private $feed; 
+
+    public function __construct($feed) {
+        $this->feed = $feed;
+    }
+
+    public function kwh_at_cop($id_elec,$id_heat,$start,$end,$histogram_div,$interval) {
+        $histogram_div = (float) $histogram_div;
+        $interval = (int) $interval;
+
+        $start = floor($start / $interval) * $interval;
+        $end = floor($end / $interval) * $interval;
+        $power_to_kwh = $interval / 3600000;
+        
+        $elec_data = $this->feed->get_data($id_elec,$start,$end,$interval,1,"UTC","notime");
+        $heat_data = $this->feed->get_data($id_heat,$start,$end,$interval,1,"UTC","notime");
+        $npoints = count($elec_data);
+        
+        $histogram = array();
+        
+        $timer = microtime(true);
+        for ($i=0; $i<$npoints; $i++) {
+            $elec = $elec_data[$i];
+            $heat = $heat_data[$i];
+            
+            if ($elec>0) {
+            
+                $cop = $heat / $elec;
+                if ($cop>1 && $cop<8) {
+                    // calculate histogram allocation
+                    $div = "".(floor($cop / $histogram_div) * $histogram_div);
+                    // add to histogram
+                    if (!isset($histogram[$div])) {
+                        $histogram[$div] = 0;
+                    }
+                    $kwh_inc = $heat * $power_to_kwh;
+                    $histogram[$div] += $kwh_inc;
+                }
+            }
+        }
+        
+        return $this->format($histogram,$histogram_div,$timer);
+    }
+    
+    function hwh_at_temperature($id_power,$id_flowT,$start,$end,$histogram_div,$interval) {
+        $histogram_div = (float) $histogram_div;
+        $interval = (int) $interval;
+        
+        $start = floor($start / $interval) * $interval;
+        $end = floor($end / $interval) * $interval;
+        $power_to_kwh = $interval / 3600000;
+        
+        $power_data = $this->feed->get_data($id_power,$start,$end,$interval,1,"UTC","notime");
+        $flowT_data = $this->feed->get_data($id_flowT,$start,$end,$interval,1,"UTC","notime");
+        $npoints = count($power_data);
+        
+        $histogram = array();
+        
+        $timer = microtime(true);
+        for ($i=0; $i<$npoints; $i++) {
+            $power = $power_data[$i];
+            $flowT = $flowT_data[$i];
+            
+            // calculate histogram allocation
+            $div = "".(floor($flowT / $histogram_div) * $histogram_div);
+            // add to histogram
+            if (!isset($histogram[$div])) {
+                $histogram[$div] = 0;
+            }
+            $kwh_inc = $power * $power_to_kwh;
+            $histogram[$div] += $kwh_inc;
+        }
+        
+        return $this->format($histogram,$histogram_div,$timer);
+    }
+    
+    
+    public function format($histogram,$histogram_div,$timer) {
+        // find largest key
+        $keys = array_keys($histogram);
+        $min = min($keys);
+        $max = max($keys);
+    
+        $output = array(
+            "min" => $min*1,
+            "max" => $max*1,
+            "div" => $histogram_div*1,
+            "data" => [],
+            "time" => number_format(microtime(true)-$timer,3,".","")
+        );
+    
+        for ($i=$min; $i<=$max; $i+=$histogram_div) {
+            if (isset($histogram["".$i])) {
+                $output["data"][] = number_format($histogram["".$i],3,".","")*1;
+            } else {
+                $output["data"][] = 0;
+            }
+        }
+        return $output;
+    }
+}
---
+++ histogram_controller.php
@@ -0,0 +1,52 @@
+<?php
+
+function histogram_controller() {
+    global $route,$settings,$mysqli,$redis,$session;
+
+    if ($route->action=="") {
+        // return view("Modules/histogram/view.php",array());
+
+    } else if ($route->action=="data") {
+        $route->format = "json";
+            
+        require_once "Modules/feed/feed_model.php";
+        $settings['feed']['max_datapoints'] = 1200000;
+        $feed = new Feed($mysqli,$redis,$settings["feed"]);
+
+        require "Modules/histogram/histogram.php";
+        $histogram = new Histogram($feed);
+        
+        $start = get("start",true);
+        $end = get("end",true);
+        $div = get("div",true);
+        $interval = get("interval",true);
+
+        if ($route->subaction=="kwh_at_cop") {
+            $elec = get("elec",true);
+            $heat = get("heat",true);
+
+            if (!$feed->read_access($session['userid'],$elec)) {
+                return array("success"=>false, "message"=>"invalid access");
+            }
+            if (!$feed->read_access($session['userid'],$heat)) {
+                return array("success"=>false, "message"=>"invalid access");
+            }
+            return $histogram->kwh_at_cop($elec,$heat,$start,$end,$div,$interval);
+
+        } else if ($route->subaction=="hwh_at_temperature") {
+            $power = get("power",true);
+            $temperature = get("temperature",true);
+
+            if (!$feed->read_access($session['userid'],$power)) {
+                return array("success"=>false, "message"=>"invalid access");
+            }
+            if (!$feed->read_access($session['userid'],$temperature)) {
+                return array("success"=>false, "message"=>"invalid access");
+            }
+            return $histogram->hwh_at_temperature($power,$temperature,$start,$end,$div,$interval);
+
+        } else {
+            return array("success"=>false, "message"=>"invalid subaction");
+        }
+    }
+}
